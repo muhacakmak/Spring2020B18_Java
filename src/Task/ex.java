5128ec6fe2cdbabe8fce436fa915955f0d4d796f
ssudemirci <selcukqa@gmail.com>
2020-04-01 01:33:27 -0400

    Tasks

--- src/Task/ex.java
+++ src/Task/ex.java
@@ -5,26 +5,36 @@ import java.util.Scanner;
 public class ex {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-//        System.out.println("Enter seconds: ");
-//        int inputSeconds = input.nextInt();
-//        int hours = (inputSeconds/60)/60;
-//        int minutes = (inputSeconds/60)%60;
-//        int seconds = (inputSeconds%60);
-//        System.out.println(hours+" hours, "+minutes+" minutes, and "+seconds+" seconds");
-        System.out.println("Enter 3 digit number: ");
-        int num= input.nextInt();
-        int num1, num3;
-        num1 = num % 10;
-        num3 = ((num/100));
-        if((num % 99 != 0) && (num / 1000 == 0)) {
-            if (num1 == num3) {
-                System.out.println("It's palindrome");
-            } else if (num1 != num3) {
-                System.out.println("It's not palindrome");
-            }
-        }else{
-            System.out.println("It's not 3 digit numner");
+        System.out.println("Enter number");
+        int numOfDay =input.nextInt();
+        String result="";
+        switch (numOfDay){
+            case 1:
+                result = "Monday";
+                break;
+            case 2:
+                result = "Tuesday";
+                break;
+            case 3:
+                result = "Wednesday";
+                break;
+            case 4:
+                result = "Thursday";
+                break;
+            case 5:
+                result = "Friday";
+                break;
+            case 6:
+                result = "Saturday";
+                break;
+            case 7:
+                result = "Sunday";
+                break;
+            default:
+                result = "Invalid day";
+                break;
         }
+        System.out.println(result);
     }
 }
 
--- src/Task/sabanAbi.java
+++
@@ -1,9 +0,0 @@
-package Task;
-
-import java.util.Scanner;
-
-public class sabanAbi {
-
-}
-
-
