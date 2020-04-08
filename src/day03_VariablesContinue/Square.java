5d3d09151d529043a6b6e54ba8c0c138e3560040
ssudemirci <selcukqa@gmail.com>
2020-04-07 23:26:29 -0400

    day03

--- src/day03_VariablesContinue/Square.java
+++ src/day03_VariablesContinue/Square.java
@@ -2,8 +2,8 @@ package day03_VariablesContinue;
 /*
 Scanner_Practice1 07:
   create a class called Sequare, and write a program that can calculate the areas
-  and perimeters of the sequare based on the length of the sequare, and print the result
-  Note: you only need to length of the sequare in order to calculate the area and perimeter
+  and perimeters of the sequare based on the lengthMethods of the sequare, and print the result
+  Note: you only need to lengthMethods of the sequare in order to calculate the area and perimeter
  */
 public class Square {
     public static void main(String[] args) {
@@ -12,8 +12,8 @@ public class Square {
         double perimeterOfSquare=length*4;
 
 
-        System.out.println("The area for the square of length "+length+ "cm  is "+areaOfSquare+" cm^2. ");
-        System.out.println("The perimeter for the square of length "+length+ " is "+perimeterOfSquare+" cm.");
+        System.out.println("The area for the square of lengthMethods "+length+ "cm  is "+areaOfSquare+" cm^2. ");
+        System.out.println("The perimeter for the square of lengthMethods "+length+ " is "+perimeterOfSquare+" cm.");
 
 
     }
