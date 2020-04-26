f2a371b8564b2f8f9dcd5fbd2bae9e78f67d6185
ssudemirci <selcukqa@gmail.com>
2020-04-26 00:29:28 -0400

    Day28 Class

---
+++ src/day28_DateTime/Time_Formatting.java
@@ -0,0 +1,19 @@
+package day28_DateTime;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class Time_Formatting {
+    public static void main(String[] args) {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("hh:mm:ss a");
+        LocalTime time1 = LocalTime.of(11, 30,30);
+        System.out.println(time1.format(dtf));
+
+        LocalTime time2=LocalTime.of(12,0,30);
+        System.out.println(time2.format(dtf));
+
+
+
+    }
+}
+
