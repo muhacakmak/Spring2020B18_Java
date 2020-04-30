fdbdae02ea0e6b4125d0d6922aab2601259efeaa
ssudemirci <selcukqa@gmail.com>
2020-04-30 16:31:50 -0400

    Day31 Class

---
+++ src/day31_BulkOperations/BulkOperations/RemoveDuplicates.java
@@ -0,0 +1,21 @@
+package day31_BulkOperations.BulkOperations;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+        ArrayList<Integer>list=new ArrayList <>();
+        list.addAll(Arrays.asList(1,1,2,2,3,3));
+
+
+        ArrayList<Integer>result=new ArrayList <>();// expexted result :[]
+        for (Integer each:list) {
+
+        }
+
+
+
+
+    }
+}
