cf133a6ff58a22c0a811b0d5dad636a79617a02f
ssudemirci <selcukqa@gmail.com>
2020-05-01 22:55:25 -0400

    Day29 Class

--- src/day29_Wrapper_ArrayList/ReviewWrapper/Auto_Unboxing.java
+++ src/day29_Wrapper_ArrayList/ReviewWrapper/Auto_Unboxing.java
@@ -1,4 +1,18 @@
 package day29_Wrapper_ArrayList.ReviewWrapper;
 
 public class Auto_Unboxing {
+    public static void main(String[] args) {
+        int a = 100;//  primitive
+
+        Integer B = a;  // Auto boxing
+        Byte Byte1 = 98;
+
+        short short1 = Byte1;// unboxing
+        byte b2 = Byte1;     // unboxing
+        int int1 = Byte1;   // unboxing
+
+
+    }
+
+
 }
---
+++ src/day29_Wrapper_ArrayList/ReviewWrapper/ParseMethods.java
@@ -0,0 +1,4 @@
+package day29_Wrapper_ArrayList.ReviewWrapper;
+
+public class ParseMethods {
+}
