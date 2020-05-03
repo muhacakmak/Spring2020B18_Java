9e43c6835cad715ea1064e3e2cbe3d9c6408ea25
ssudemirci <selcukqa@gmail.com>
2020-05-03 00:36:11 -0400

    ...

--- src/CodinBat/String_1/makeOutWord.java
+++
@@ -1,4 +0,0 @@
-package CodinBat;
-
-public class makeOutWord {
-}
---
+++ src/day30_ArrayList/review/RemoveMethods2.java
@@ -0,0 +1,13 @@
+package day30_ArrayList.review;
+import java.util.ArrayList;
+
+public class RemoveMethods2 {
+    public static void main(String[] args) {
+        ArrayList <String> list = new ArrayList <>();
+
+
+
+
+
+    }
+}
