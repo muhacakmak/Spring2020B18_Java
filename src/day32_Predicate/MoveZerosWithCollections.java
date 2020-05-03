5a5dfdb276c975461a28b83e18da744aba481aa1
ssudemirci <selcukqa@gmail.com>
2020-05-03 15:35:21 -0400

    Day32 class

--- src/day32_Predicate/MoveZerosWithCollections.java
+++ src/day32_Predicate/MoveZerosWithCollections.java
@@ -2,8 +2,9 @@ package day32_Predicate;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
-public class MoveZeros {
+public class MoveZerosWithCollections {
     /*
      Write a program that can move all the zeros to  last indexes of ArrayList
         Ex:
@@ -15,21 +16,16 @@ public class MoveZeros {
     public static void main(String[] args) {
 
         ArrayList <Integer> list = new ArrayList <>(Arrays.asList(1, 0, 2, 0, 3, 0, 4, 0));
-        System.out.println("Original list : "+list);
 
-        int count = 0;
-        for (int i = 0; i < list.size(); i++) {
-            if(list.get(i)==0){
-                count++;
-            }
-        }
+        System.out.println("Original list : "+list);
+        int count=Collections.frequency(list,0);
         System.out.println("Count of zero : "+count);
-        list.removeAll(Arrays.asList(0));
+        Integer zero=0;
+        list.removeAll(Arrays.asList(zero));
         System.out.println("After remove zero :"+list);
         for (int i = 0; i <count ; i++) {
             list.add(0);
         }
-
         System.out.println("After add Zero end: "+list);
 
 
