d8d548711f1f5b8f35a4f32bd26823a31fbb795a
ssudemirci <selcukqa@gmail.com>
2020-05-03 18:43:36 -0400

    Day32 Class

---
+++ src/day32_Predicate/Uniques.java
@@ -0,0 +1,38 @@
+package day32_Predicate;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Uniques {
+    /*
+    write a program that can return the unique objects from arraylist of characters
+
+     */
+    public static void main(String[] args) {
+        ArrayList <Character> list = new ArrayList <>(Arrays.asList('A', 'A', 'B', 'C', 'D', 'D'));
+        ArrayList <Character> result = new ArrayList <>(Arrays.asList());
+        System.out.println("Original list: " + list);
+
+/*
+        for (int i = 0; i <list.size() ; i++) {
+            int count= Collections.frequency(list,list.get(i));
+            if(count==1){
+                result.add(list.get(i));
+            }
+        }
+
+ */
+
+        for (Character each:list) {
+            int count= Collections.frequency(list,each);
+            if(count==1){
+                result.add(each);
+            }
+        }
+
+        System.out.println(result);
+
+
+    }
+}
