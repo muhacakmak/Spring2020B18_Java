0dcbc625953a0d33069d6aa14feb4c834bf48025
ssudemirci <selcukqa@gmail.com>
2020-05-03 01:38:54 -0400

    Day31 Class

--- src/day30_ArrayList/review/RemoveMethods2.java
+++ src/day30_ArrayList/review/RemoveMethods2.java
@@ -4,6 +4,23 @@ import java.util.ArrayList;
 public class RemoveMethods2 {
     public static void main(String[] args) {
         ArrayList <String> list = new ArrayList <>();
+        list.add("A");
+        list.add("B");
+        list.add("C");
+        System.out.println(list);
+        //list.remove(1);
+
+        String str="Z";
+       boolean r1= list.remove(str);
+
+       boolean r2=list.remove("A");
+        System.out.println(r1);
+        System.out.println(r2);
+        System.out.println(list);
+        list.clear();
+        System.out.println(list);
+
+
 
 
 
