34c99fb378012d89820adca0c377e208688d67ca
ssudemirci <selcukqa@gmail.com>
2020-05-03 18:20:19 -0400

    Day32 Class

--- src/day32_Predicate/ReplaceAllMethods.java
+++ src/day32_Predicate/ReplaceAllMethods.java
@@ -1,4 +1,23 @@
 package day32_Predicate;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class ReplaceAllMethods {
+    public static void main(String[] args) {
+        ArrayList<String>names=new ArrayList <>(Arrays.asList("Omer","Omer","Mary","Sha","Fatih","Omer"));// Irina
+        System.out.println("Original list: "+names);
+
+//        names.set(0,"Irina");
+//        names.set(1,"Irina");
+//        names.set(5,"Irina");
+        Collections.replaceAll(names,"Omer","Irina");
+
+
+
+        System.out.println(names);
+
+
+    }
 }
