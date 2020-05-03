85fa20d46ba3d1d57e21f9e30c20d2b24745cf97
ssudemirci <selcukqa@gmail.com>
2020-05-03 05:41:25 -0400

    Day31 Class

--- src/day31_BulkOperations/BulkOperations/RemoveDuplicates.java
+++ src/day31_BulkOperations/BulkOperations/RemoveDuplicates.java
@@ -11,9 +11,29 @@ public class RemoveDuplicates {
 
         ArrayList<Integer>result=new ArrayList <>();// expexted result :[]
         for (Integer each:list) {
+            if(!result.contains(each)){
+
+                result.add(each);
 
 
-
+            }
         }
+        System.out.println(result);
+
+        System.out.println("======================================");
+        Integer[]arr1={1,1,2,2,3,3};
+        ArrayList<Integer>nonDuplicates=new ArrayList <>();
+        for (int i = 0; i <arr1.length ; i++) {
+            if(!nonDuplicates.contains(arr1[i])){
+                nonDuplicates.add(arr1[i]);
+            }
+        }
+        System.out.println(nonDuplicates);
+
+
+
+
+
+
     }
 }
