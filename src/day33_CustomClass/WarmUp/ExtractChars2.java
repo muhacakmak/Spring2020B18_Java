0fa934cb5e64a6e0af333485b82bec8fe4c81bb1
ssudemirci <selcukqa@gmail.com>
2020-05-04 10:52:14 -0400

    Day33 Class

---
+++ src/day33_CustomClass/WarmUp/ExtractChars2.java
@@ -0,0 +1,39 @@
+package day33_CustomClass.WarmUp;
+
+import java.util.ArrayList;
+
+public class ExtractChars2 {
+    /*
+    3. write a program that can extract the sepecial characters,
+    digits and alphebets from a string and stores them into seperate ArrayLists of Character:
+                			ex:
+                				str = "ABCD123$%#@&456EFG!";
+                				list1: {$, %, #, @, &, !}
+                				list2: {A, B, C, D, E, F, G}
+                				list3: {1, 2, 3, 4, 5, 6}
+     */
+    public static void main(String[] args) {
+        String str = "ABCD123$%#@&456EFG!";
+        char[] arr = str.toCharArray();
+
+        ArrayList <Character> letters = new ArrayList <>();
+        ArrayList <Character> digit = new ArrayList <>();
+        ArrayList <Character> specialChars = new ArrayList <>();
+
+        for (int i = 0; i <str.length() ; i++) {
+            char each=str.charAt(i);
+
+            if (Character.isLetter(each)) {  //
+                letters.add(each);
+            } else if (Character.isDigit(each)) {
+                digit.add(each);
+            } else {
+                specialChars.add(each);
+            }
+
+        }
+        System.out.println(letters);
+        System.out.println(digit);
+        System.out.println(specialChars);
+    }
+}
