39b14ddfcd0b33fab23e67e0586369168d054727
ssudemirci <selcukqa@gmail.com>
2020-05-07 13:06:06 -0400

    Day35 Class

---
+++ src/day35_Static/StudentObjects.java
@@ -0,0 +1,15 @@
+package day35_Static;
+
+public class StudentObjects {
+    public static void main(String[] args) {
+        Student student1=new Student();
+        student1.name="Muhtar";
+
+        Student student2=new Student();
+        student2.name="Kuzat";
+
+        System.out.println(student1);
+        System.out.println(student2);
+
+    }
+}
