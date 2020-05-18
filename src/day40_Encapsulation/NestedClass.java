746dc64d68c0b075edf8db2358ee99650a504a4d
ssudemirci <selcukqa@gmail.com>
2020-05-18 05:23:17 -0400

    Day40 Class

---
+++ src/day40_Encapsulation/NestedClass.java
@@ -0,0 +1,13 @@
+package day40_Encapsulation;
+
+public class NestedClass {
+
+    static class data{
+
+
+    }
+
+    public static void main(String[] args) {
+       // NestedClass.data;
+    }
+}
